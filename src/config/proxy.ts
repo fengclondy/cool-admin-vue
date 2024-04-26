a9794a8b4deb2f72de45f138635e1ab43e8ccd57
fengclondy <fengclondy@126.com>
2024-04-26 10:16:28 +0800

    Update proxy.ts

--- src/config/proxy.ts
+++ src/config/proxy.ts
@@ -1,12 +1,12 @@
 export const proxy = {
 	"/dev/": {
-		target: "http://127.0.0.1:8001",
+		target: "http://10.34.4.38:8081",
 		changeOrigin: true,
 		rewrite: (path: string) => path.replace(/^\/dev/, "")
 	},
 
 	"/prod/": {
-		target: "https://show.cool-admin.com",
+		target: "http://10.34.4.38:8081",
 		changeOrigin: true,
 		rewrite: (path: string) => path.replace(/^\/prod/, "/api")
 	}
