c00784ee37e5010d81999969d46c6d86e1fbedc1
fengclondy <fengclondy@126.com>
2024-04-26 11:16:25 +0800

    Update proxy.ts

--- src/config/proxy.ts
+++ src/config/proxy.ts
@@ -6,7 +6,7 @@ export const proxy = {
 	},
 
 	"/prod/": {
-		target: "https://aibot2-104289-5-1259062193.sh.run.tcloudbase.com",
+		target: "https://hvcughet.aibot2.64yhl95u.dhc5yvzt.com:8001",
 		changeOrigin: true,
 		rewrite: (path: string) => path.replace(/^\/prod/, "/api")
 	}
