a27e6d09b5614228f32daeb87b37641defaa9e2d
fengclondy <fengclondy@126.com>
2024-04-26 12:28:44 +0800

    Update proxy.ts

--- src/config/proxy.ts
+++ src/config/proxy.ts
@@ -1,12 +1,12 @@
 export const proxy = {
 	"/dev/": {
-		target: "http://hvcughet.aibot2.64yhl95u.dhc5yvzt.com",
+		target: "https://hvcughet.aibot2.64yhl95u.dhc5yvzt.com:8001/",
 		changeOrigin: true,
 		rewrite: (path: string) => path.replace(/^\/dev/, "")
 	},
 
 	"/prod/": {
-		target: "https://hvcughet.aibot2.64yhl95u.dhc5yvzt.com",
+		target: "https://hvcughet.aibot2.64yhl95u.dhc5yvzt.com:8001/",
 		changeOrigin: true,
 		rewrite: (path: string) => path.replace(/^\/prod/, "/api")
 	}
